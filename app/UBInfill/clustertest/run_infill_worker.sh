be31bce0f30737c428b03d3f6a0413ba02e5bb19
kmason03 <masonkatie31@gmail.com>
2019-05-13 16:10:39 +0000

    fix for tufts cluster

--- app/UBInfill/clustertest/run_infill_worker.sh
+++ app/UBInfill/clustertest/run_infill_worker.sh
@@ -6,7 +6,7 @@ source configure.sh
 cd scripts
 source setenv_ublarcvserver.sh
 
-cd ublarcvserver
+cd ../ublarcvserver
 source configure.sh
 cd app/UBInfill/clustertest
 pwd
@@ -17,7 +17,6 @@ devnum=`sed -n ${line}p /cluster/tufts/wongjiradlab/kmason03/uboonecode/ubdl/ubl
 device=`printf cuda:%d ${devnum}`
 logfile=`printf /tmp/worker_id%d.log ${SLURM_ARRAY_TASK_ID}`
 script=/cluster/tufts/wongjiradlab/kmason03/uboonecode/ubdl/ublarcvserver/app/UBInfill/clustertest/start_ublarcvserver_infill_worker.py
-#scrpit=start_ublarcvserver_worker.py
 echo ${device}
 cmd="python ${script} -l ${logfile} -d tcp://localhost:6000 -m ${device} -w ${weightdir}"
 
