bac5405166785809e9c8af1123b12e2c96e05ab8
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-07-01 12:34:33 -0400

    added cluster scripts. also modify configuration script for environment variables

--- app/ubmrcnn/setenv.sh
+++ app/ubmrcnn/setenv.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # location of this script
-where="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+where="$( cd "$( dirname "${PWD}/${BASH_SOURCE[0]}" )" && pwd )"
 
 # add model folder to python path
-export UBMRCNN_MODEL_DIR=${UBLARCVSERVER_BASEDIR}/networks/ubmrcnn/models
-export UBMRCNN_WEIGHT_DIR=${UBLARCVSERVER_BASEDIR}/networks/ubmrcnn/weights
 export UBMRCNN_SERVERAPP_DIR=$where
 
 # add detectron folder to python path
@@ -13,23 +11,18 @@ export MASKRCNN_DIR=${UBLARCVSERVER_BASEDIR}/networks/mask-rcnn.pytorch
 
 # add to python path
 
-# 1) model
-[[ ":$PYTHONPATH:" != *":${UBMRCNN_MODEL_DIR}:"* ]] && \
-    export PYTHONPATH="${UBMRCNN_MODEL_DIR}:${PYTHONPATH}"
-
-# 2) server app
+# 1) server app
 [[ ":$PYTHONPATH:" != *":${UBMRCNN_SERVERAPP_DIR}:"* ]] && \
     export PYTHONPATH="${UBMRCNN_SERVERAPP_DIR}:${PYTHONPATH}"
 
-# 3a) detectron
+# 2a) detectron
 [[ ":$PYTHONPATH:" != *":${MASKRCNN_DIR}:"* ]] && \
     export PYTHONPATH="${MASKRCNN_DIR}:${PYTHONPATH}"
 
-# 3b) detectron lib
+# 2b) detectron lib
 [[ ":$PYTHONPATH:" != *":${MASKRCNN_DIR}/lib:"* ]] && \
     export PYTHONPATH="${MASKRCNN_DIR}/lib:${PYTHONPATH}"
 
-
-# PATH
+# 3) PATH
 [[ ":$PATH:" != *":${UBMRCNN_SERVERAPP_DIR}:"* ]] && \
     export PATH="${UBMRCNN_SERVERAPP_DIR}:${PATH}"
---
+++ app/ubmrcnn/tufts_cluster_scripts/submit_ubmrcnn_worker_pgpu03.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#SBATCH --job-name=ubmrcnn_worker_pgpu03
+#SBATCH --output=slurmout_ubmrcnn_worker.log
+#SBATCH --mem-per-cpu=8000
+#SBATCH --time=3-0:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --partition gpu
+#SBATCH --nodelist=pgpu03
+#SBATCH --array=0
+
+CONTAINER=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_py3_deponly_070119.img
+
+
+# UBDL_DIR: location of ubdl dependencies (e.g. larlite, larcv, ublarcvapp)
+UBDL_DIR=/cluster/tufts/wongjiradlab/twongj01/py3_ubdl
+#UBDL_DIR=/usr/local/ubdl # Container copy
+
+# UBMRCNN_DIR: location of UB Mask-RCNN scripts
+UBMRCNN_DIR=/cluster/tufts/wongjiradlab/twongj01/py3_ubdl/ublarcvserver/app/ubmrcnn
+#UBMRCNN_DIR=${UBDL_DIR}/ublarcvserver/app/ubmrcnn  # Container copy
+
+# WORKING DIRECTORY
+WORKDIR=${UBMRCNN_DIR}/tufts_cluster_scripts
+
+# WEIGHT DIR
+WEIGHT_DIR=/cluster/tufts/wongjiradlab/larbys/dl_models/ubmrcnn_mcc8_v1
+
+# WORK LOG
+WORKER_LOG=${WORKDIR}/worker_ubmrcnn.log
+
+# IP ADDRESSES OF BROKER
+#BROKER=10.246.81.73 # PGPU03
+BROKER=nudot.lns.mit.edu
+
+PORT=6000
+
+# GPU LIST
+GPU_ASSIGNMENTS=${WORKDIR}/tufts_pgpu03_assignments.txt
+
+COMMAND="source ${WORKDIR}/run_ubmrcnn_worker.sh tcp://${BROKER}:${PORT} ${UBMRCNN_DIR} ${UBDL_DIR} ${WEIGHT_DIR} ${GPU_ASSIGNMENTS} ${WORKER_LOG}"
+
+module load singularity
+singularity exec --nv ${CONTAINER} bash -c "${COMMAND}"
