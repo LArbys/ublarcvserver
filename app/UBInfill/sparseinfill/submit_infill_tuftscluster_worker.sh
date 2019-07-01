fb8c7d1ba1ae90ca6d53a9dcd0b4d893e5eca982
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-07-01 13:44:46 -0400

    mod to infill worker submit script

--- app/UBInfill/sparseinfill/submit_infill_tuftscluster_worker.sh
+++ app/UBInfill/sparseinfill/submit_infill_tuftscluster_worker.sh
@@ -7,29 +7,31 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition gpu
 #SBATCH --nodelist=pgpu02
-#SBATCH --array=0
+#SBATCH --array=[2-3]
 
 CONTAINER=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_051919.img
 
 # UBDL_DIR: location of dependencies (e.g. larcv, larlite, ublarcvapp)
 #UBDL_DIR=/cluster/tufts/wongjiradlab/kmason03/testdir/ubdl
-UBDL_DIR=/cluster/tufts/wongjiradlab/twongj01/py3_ubdl
+UBDL_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl
 #UBDL_DIR=/usr/local/ubdl # In container
 
 # INFILL_DIR: location of ublarcvserver Infill app directory
-INFILL_DIR=/cluster/tufts/wongjiradlab/twongj01/py3_ubdl/ublarcvserver/app/UBInfill
+INFILL_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl/ublarcvserver/app/UBInfill
 
 # WEIGHT_DIR: location of weights
 WEIGHT_DIR=/cluster/tufts/wongjiradlab/larbys/dl_models/sparse_infill/prelim_june2019/
 
-# GPU_LIST
-GPU_LIST=/cluster/tufts/wongjiradlab/kmason03/testdir/ubdl/ublarcvserver/app/UBInfill/sparseinfill/tufts_pgpu03_assignments.txt
-
 # LOG_FILE
 LOG_DIR=/tmp/
 
-SSS_BASEDIR=/usr/local/ubdl
+# WORKDIR: place for scripts
 WORKDIR=${INFILL_DIR}/sparseinfill
+
+# GPU_LIST
+#GPU_LIST=/cluster/tufts/wongjiradlab/kmason03/testdir/ubdl/ublarcvserver/app/UBInfill/sparseinfill/tufts_pgpu03_assignments.txt
+GPU_LIST=${WORKDIR}/tufts_pgpu03_assignments.txt
+
 
 # IP ADDRESSES OF BROKER
 #BROKER=10.246.81.73 # PGPU03
